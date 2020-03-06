a4d90e310bc5eed573a7289c0b152485ffcee9b7
Jonathan <38326991+jonathanamorim@users.noreply.github.com>
2020-03-05 23:16:45 -0300

    Update and rename horas-dias02.js to horas-dias.js

---
+++ horas-dias.js
@@ -0,0 +1,4 @@
+var $entrada = prompt('digite as horas')
+var $dias = parseInt($entrada/24)
+var $horas = parseInt($entrada%24)
+alert($dias+' dias e '+$horas+' horas')
--- horas-dias02.js
+++
@@ -1,4 +0,0 @@
-var $entrada = prompt('digite as horas')
-var $dias = parseInt($entrada/24)
-var $horas = parseFloat($entrada%24)
-alert($entrada+' horas = '+$dias+' dias e '+$horas.toFixed(2)+' horas.')
