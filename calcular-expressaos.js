d041edec0b3c43816c547a677d9d37f8aaf52c7f
Jonathan <38326991+jonathanamorim@users.noreply.github.com>
2020-03-15 16:47:29 -0300

    Add files via upload

---
+++ calcular-expressaos.js
@@ -0,0 +1,11 @@
+var $a = 1.5 
+var $b = 4 
+var $c = 2 
+var $d = 3 
+var $e = 1.2 
+var $f = 4.3 
+var $c1 = ($a*($c+$d))/($b*($e+$f))
+var $c2 = ((Math.pow($a,$b+$c))/($e+$f))+($d)
+var $c3 = ((($a)+(Math.pow($c+$d,2)/(Math.pow($b,2)))*($d))*(1/$c))
+var $c4 = ((-$b+Math.sqrt($b*$b-4*$a*$c))/(2))
+console.log('a) '+$c1+'\n'+'b) '+$c2+'\n'+'c) '+$c3+'\n'+'d) '+$c4)
