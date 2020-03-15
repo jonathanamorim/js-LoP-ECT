e7d19e530b387ad10258abb8a95292fef3342539
Jonathan <38326991+jonathanamorim@users.noreply.github.com>
2020-03-15 16:45:52 -0300

    Add files via upload

---
+++ udc.js
@@ -0,0 +1,7 @@
+var $entrada = parseInt(prompt('valor: '))
+var $v1 = parseInt(($entrada / 100))
+var $entrada = ($entrada-$v1*100)
+var $v2 = parseInt(($entrada / 10))
+var $entrada = ($entrada-$v2*10)
+var $v3 = parseInt($entrada)
+alert($v3+''+$v2+''+$v1)
